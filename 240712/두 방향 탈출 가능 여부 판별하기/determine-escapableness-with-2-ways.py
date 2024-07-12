4401556aa82e3386fd722c248e95c78ad11b2827
Juhui <78090753+socrativejh@users.noreply.github.com>
2024-07-12 14:55:51 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 131ms, Memory: 27MB, Status: Passed - CodeTree

--- 240712/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240712/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -2,8 +2,8 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-graph = [] * (n)
-visited = [[False] * (n) for _ in range(m)]
+graph = [] * (m)
+visited = [[False] * (m) for _ in range(n)]
 
 dx = [1, 0]
 dy = [0, 1]
@@ -12,7 +12,7 @@ for i in range(n):
     graph.append(list(map(int, input().split())))
 
 def cango(x, y):
-    if x < 0 or x >= m or y < 0 or y >= n: return False
+    if x < 0 or x >= n or y < 0 or y >= m: return False
     if not graph[x][y] or visited[x][y]: return False
     return True
  
@@ -22,7 +22,7 @@ def dfs(x, y):
         nx = x + dx[i]
         ny = y + dy[i]
         if cango(nx, ny):
-            if nx == m-1 and ny == n-1: 
+            if nx == n-1 and ny == m-1: 
                 flag = 1
             visited[nx][ny] = True
             dfs(nx, ny)
