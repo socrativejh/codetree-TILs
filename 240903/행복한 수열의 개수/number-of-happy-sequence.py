8e4a257261e2ed387c55372a9bb78783ff988cab
Juhui <78090753+socrativejh@users.noreply.github.com>
2024-09-03 20:36:44 +0900

    Title: 행복한 수열의 개수, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 240903/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240903/행복한 수열의 개수/number-of-happy-sequence.py
@@ -7,12 +7,19 @@ seq1 = []; seq2 = []; num = 0
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
+# 끊어진 후 부분에서 더 길게 연속될 수 있음 (max_cnt 선언)
 def happy(seq):
     cnt = 1
+    max_cnt = 1
     for i in range(n-1):
         if seq[i] == seq[i+1]:
             cnt += 1
-    if cnt >= m:
+        else:
+            if max_cnt < cnt: 
+                max_cnt = cnt
+            cnt = 1
+    if max_cnt < cnt: max_cnt = cnt
+    if max_cnt >= m:
         return True
     return False
 
